26cb9094f14bbdd9d91a799ffc7d671790e687c4
Samir Hinojosa <samirhinojosa@gmail.com>
2021-10-29 17:10:45 +0200

    refactoring df_analysis

--- functions.py
+++ functions.py
@@ -4,20 +4,8 @@ import pandas as pd
 from math import prod
 
 
-# print(100 * '_')
-# print("{:<15} {:<10} {:<8} {:<10} {:<20} {:<16} {:<11}".format("dataset", "clusters", "time", 
-#                                                                "inertia", "calinski-harabasz", 
-#                                                                "davies-bouldin", "silhouette"))
-
-# # kmeans = KMeans(init="k-means++", n_clusters=4, max_iter=1000, random_state=10)
-# benchmark_kmeans("PCA-reduced", kmeans_reduced, n_clusters=4, data=df_pca_reduced.values)
-
-# # kmeans = KMeans(init="k-means++", n_clusters=9, max_iter=1000, random_state=10)
-# benchmark_kmeans("PCA-complete", kmeans_complete, n_clusters=9, data=df_pca_complete.values)
-
-
-
-def df_analysis(df, name_df, *args, **kwargs):
+def df_analysis(df, name_df,
+                *args, **kwargs):
     """
     Method used to analyze on the DataFrame.
 
@@ -25,110 +13,114 @@ def df_analysis(df, name_df, *args, **kwargs):
     -----------------
         df (pandas.DataFrame): Dataset to analyze
         name_df (str): Dataset name
-        
+
         *args, **kwargs:
         -----------------
             columns (list): Dataframe keys in list format
-            flag (str): Flag to show complete information about the dataset to analyse
-                        "complete" shows all information about the dataset
+            flag (str): Flag to show complete information about
+                        the dataset to analyse "complete" shows
+                        all information about the dataset
 
     Returns:
     -----------------
-        None. 
-        Print the analysis on the Dataset. 
+        None.
+        Print the analysis on the Dataset.
     """
-    
+
     # Getting the variables
     columns = kwargs.get("columns", None)
-    type_analysis = kwargs.get("type_analysis", None)
-    
-    ORDERING_COMPLETE = [
-        "name", "type", "records", "unique", "# NaN", "% NaN", "mean", "min", "25%", "50%", "75%", "max", "std"
+
+    # Getting the variables
+    # tha analysis type are ["complete", "header"]
+    # By defaut "complete"
+    analysis_type = kwargs.get("analysis_type", None)
+
+    ordered_columns = [
+        "name", "type", "records", "unique",
+        "# NaN", "% NaN", "mean", "min",
+        "25%", "50%", "75%", "max", "std"
     ]
-    
+
     # Calculating the memory usage based on dataframe.info()
     buf = io.StringIO()
     df.info(buf=buf)
-    memory_usage = buf.getvalue().split('\n')[-2]
-    
+    memory_usage = buf.getvalue().split("\n")[-2]
+
     if df.empty:
         print("The", name_df, "dataset is empty. Please verify the file.")
     else:
-        empty_cols = [col for col in df.columns if df[col].isna().all()] # identifying empty columns
-        df_rows_duplicates = df[df.duplicated()] #identifying full duplicates rows
-        
+        empty_cols = [col for col in df.columns
+                      if df[col].isna().all()]  # identifying empty columns
+        df_rows_duplicates = df[df.duplicated()]  # identifying duplicates rows
+
         # Creating a dataset based on Type object and records by columns
-        type_cols = df.dtypes.apply(lambda x: x.name).to_dict() 
-        df_resume = pd.DataFrame(list(type_cols.items()), columns = ["name", "type"])
+        type_cols = df.dtypes.apply(lambda x: x.name).to_dict()
+        df_resume = pd.DataFrame(list(type_cols.items()),
+                                 columns=["name", "type"])
         df_resume["records"] = list(df.count())
         df_resume["unique"] = list(df.nunique())
         df_resume["# NaN"] = list(df.isnull().sum())
-        df_resume["% NaN"] = list(((df.isnull().sum() / len(df.index))*100).round(2))
-        
-        print("\nAnalysis of", name_df, "dataset")
-        print(70 * '_')
-        print("- Dataset shape:                 ", df.shape[0], "rows and", df.shape[1], "columns")
-        print("- Total of NaN values:           ", df.isna().sum().sum())
-        print("- Percentage of NaN:             ", round((df.isna().sum().sum() / prod(df.shape)) * 100, 2), "%")
-        print("- Total of full duplicates rows: ", df_rows_duplicates.shape[0])
-        print("- Total of empty rows:           ", df.shape[0] - df.dropna(axis="rows", how="all").shape[0]) if df.dropna(axis="rows", how="all").shape[0] < df.shape[0] else \
-                    print("- Total of empty rows:            0")
-        print("- Total of empty columns:        ", len(empty_cols))
-        print("  + The empty column is:         ", empty_cols) if len(empty_cols) == 1 else \
-                    print("  + The empty column are:         ", empty_cols) if len(empty_cols) >= 1 else None
-        print("- Unique indexes:                ", df.index.is_unique)
-        
-        if columns is not None:
-            print("\n- The key(s):", columns, "is not present multiple times in the dataframe.\n  It CAN be used as a primary key.") if df.size == df.drop_duplicates(columns).size else \
-                print("\n- The key(s):", columns, "is present multiple times in the dataframe.\n  It CANNOT be used as a primary key.")
-            
-        if type_analysis == "summarized":
-            print("\n")
-        
-        if type_analysis is None or type_analysis != "summarized":
-            
-            pd.set_option("display.max_rows", None) # show full of showing rows
-            pd.set_option("display.max_columns", None) # show full of showing cols
-            pd.set_option("display.max_colwidth", None) # show full width of showing cols
-            pd.set_option("display.float_format", lambda x: "%.5f" % x) # show full content in cell    
-            
-            if type_analysis is None or type_analysis != "complete":
-                
-                print("\n- Type object and records by columns      (",memory_usage,")")
-                print(70 * '_')
-                
-            elif type_analysis == "complete" and (df.select_dtypes(["int64"]).shape[1] > 0 or df.select_dtypes(["float64"]).shape[1] > 0):
-                
-                df_resume["unique"] = list(df.nunique())
-                df_desc = pd.DataFrame(df.describe().T).reset_index()
-                df_desc = df_desc.rename(columns={"index": "name"})
-                df_resume = df_resume.merge(right=df_desc[["name", "mean", "min", "25%", "50%", "75%", "max", "std"]], on="name", how="left")
-                df_resume = df_resume[ORDERING_COMPLETE]
-                print("\n- Type object and records by columns      (",memory_usage,")")
-                print(70 * '_')
-            
-            display(df_resume.sort_values("records", ascending=False))
-            
-            pd.reset_option("display.max_rows") # reset max of showing rows
-            pd.reset_option("display.max_columns") # reset max of showing cols
-            pd.reset_option("display.max_colwidth") # reset width of showing cols
-            pd.reset_option("display.float_format") # reset show full content in cell
-            
-        # deleting dataframe to free memory
-        if type_analysis == "complete":
-            
-            if df.select_dtypes(["int64"]).shape[1] > 0 or df.select_dtypes(["float64"]).shape[1] > 0:
-                del [[df_resume, df_desc]]
-            else:
-                del [[df_resume]]
-            
-            gc.collect()
-            df_resume, df_desc = (pd.DataFrame() for i in range(2))
+        df_resume["% NaN"] = list(((df.isnull().sum()
+                                    / len(df.index))*100).round(2))
+
+        # Printing the analysis header
+        print("\nAnalysis Header of", name_df, "dataset")
+        print(70*"-")
+
+        print("- Dataset shape:\t\t\t", df.shape[0], "rows and",
+              df.shape[1], "columns")
+        print("- Total of NaN values:\t\t\t", df.isna().sum().sum())
+        print("- Percentage of NaN:\t\t\t",
+              round((df.isna().sum().sum()/prod(df.shape))
+                    * 100, 2), "%")
+        print("- Total of full duplicates rows:\t",
+              df_rows_duplicates.shape[0])
+
+        if df.dropna(axis="rows", how="all").shape[0] < df.shape[0]:
+            print("- Total of empty rows:\t\t\t",
+                  df.shape[0] - df.dropna(axis="rows", how="all"))
         else:
-            del df_resume
-            gc.collect()
-            df_resume = pd.DataFrame()
-            
-            
-            
-            
+            print("- Total of empty rows:\t\t\t", "0")
+
+        print("- Total of empty columns:\t\t", len(empty_cols))
+
+        if len(empty_cols) == 1 or len(empty_cols) >= 1:
+            print("\t+ The empty column is:\t\t", empty_cols)
+        else:
+            None
+
+        print("- Unique indexes:\t\t\t", df.index.is_unique)
+        print("- Memory usage:\t\t\t\t",
+              memory_usage.split("memory usage: ")[1])
+
+        if columns is not None:
+            if df.size == df.drop_duplicates(columns).size:
+                print("\n- The key(s):", columns, "is not present\
+                      multiple times in the dataframe.\n\
+                      It CAN be used as a primary key.")
+            else:
+                print("\n- The key(s):", columns, "is present multiple\
+                      times in the dataframe.\n  It CANNOT be used as a\
+                      primary key.")
+
+        # Enabling the visualizacion of
+        # all columns, rows, cell and floar format
+        print("\n")
+        pd.set_option("display.max_rows",
+                      None)  # show full of showing rows
+        pd.set_option("display.max_columns",
+                      None)  # show full of showing cols
+        pd.set_option("display.max_colwidth",
+                      None)  # show full width of showing cols
+        pd.set_option("display.float_format",
+                      lambda x: "%.5f" % x)  # show full float in cell
+
+        if analysis_type == "complete" or analysis_type is None:
+            pass
+        elif analysis_type == "header":
+            pass
+
+        pd.reset_option("display.max_rows")  # reset max of showing rows
+        pd.reset_option("display.max_columns")  # reset max of showing cols
+        pd.reset_option("display.max_colwidth")  # reset width of showing cols
+        pd.reset_option("display.float_format")  # reset float format in cell
