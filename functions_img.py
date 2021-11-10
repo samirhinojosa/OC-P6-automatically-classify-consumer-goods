e6de8806b87369108ddf2c415aecbca7ea60efc3
Samir Hinojosa <samirhinojosa@gmail.com>
2021-11-10 08:14:28 +0100

    resize images

--- functions_img.py
+++ functions_img.py
@@ -1,57 +1,35 @@
 import cv2
 
 
-def image_resize(image, width=None, height=None, inter):
+def thumbnail_image(image_name, basewidth=None, path=None):
+    """
+    Method used to create a thumbnail image
+
+    Parameters:
+    -----------------
+        image_name (str): Image name
+        basewidth (str): Image of thumbnail
+        path (str): Image path
+
+    Returns:
+    -----------------
+        Image saved in path + thumbnails
+
+    """
+
+    # reading the image and ist attributes
+    image = cv2.imread(path + image_name, cv2.IMREAD_UNCHANGED)
+    h, w = image.shape[:2]
     
-    
-    
-    
-def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
-    # initialize the dimensions of the image to be resized and
-    # grab the image size
-    dim = None
-    (h, w) = image.shape[:2]
-
-    # if both the width and height are None, then return the
-    # original image
-    if width is None and height is None:
-        return image
-
-    # check to see if the width is None
-    if width is None:
-        # calculate the ratio of the height and construct the
-        # dimensions
-        r = height / float(h)
-        dim = (int(w * r), height)
-
-    # otherwise, the height is None
-    else:
-        # calculate the ratio of the width and construct the
-        # dimensions
-        r = width / float(w)
-        dim = (width, int(h * r))
-
-    # resize the image
-    resized = cv2.resize(image, dim, interpolation = inter)
-
-    # return the resized image
-    return resized
-
-
-def thumbnail_image(image):
-    
-    basewidth = 224
-    
-    # Calculating the new size considering its aspect ratio
-    width = (basewidth/float(image.shape[1]))
-    height = int((float(image.shape[0])*float(wpercent)))
-    dim = (width, height)
+    # Calculating the size to preserve aspect ratio
+    r = basewidth / float(h)
+    dim = (int(w*r), basewidth)
     
     # Resize image
-    resized = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
+    image_resized = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
     
-    # Saving new image
-    cv2.imwrite(ORIGINAL_IMAGES_PATH + "thumbnail", resized) 
+    # Saving the new image
+    cv2.imwrite(path + "thumbnails/" + image_name, image_resized)
     
     
     
